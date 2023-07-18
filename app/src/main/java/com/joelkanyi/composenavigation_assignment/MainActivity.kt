194fed2885fd9335445840330c84e0fcb72d8c91
Washington <lokalawashington2019@gmail.com>
2023-07-18 14:20:12 +0300

    Assignment done

--- app/src/main/java/com/joelkanyi/composenavigation_assignment/MainActivity.kt
+++ app/src/main/java/com/joelkanyi/composenavigation_assignment/MainActivity.kt
@@ -3,24 +3,33 @@ package com.joelkanyi.composenavigation_assignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -34,6 +43,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+
+
             ComposeNavigationAssignmentTheme {
                 val navController = rememberNavController()
 
@@ -80,8 +92,8 @@ fun DetailsScreen(navController: NavController) {
     TopAppBar(
         title = { Text(text = "Details", style = TextStyle(color = Color.White, fontSize = 32.sp)) },
         actions = {
-            IconButton(onClick = { /* Define your action here */ }) {
-                Icon(Icons.Filled.Search, contentDescription = null)
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
             }
         }
     )
@@ -102,13 +114,14 @@ fun DetailsScreen(navController: NavController) {
 
 @Composable
 fun RatingScreen(navController: NavController) {
+    val rate = painterResource(id = R.drawable.rate)
     TopAppBar(
         title = { Text(text = "Ratings", style = TextStyle(color = Color.White, fontSize = 32.sp)) },
         actions = {
             IconButton(onClick = {
-
+                navController.navigateUp()
             }) {
-                Icon(Icons.Filled.Search, contentDescription = null)
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
             }
         }
     )
@@ -118,6 +131,39 @@ fun RatingScreen(navController: NavController) {
             .fillMaxSize()
             .padding(horizontal = 50.dp)
     ) {
-        Text(text = "Rate")
+        Column() {
+            RateCard(
+                painter = rate,
+                contentDescription = null,
+            )
+            Text("Rating 4.5", style = TextStyle(color = Color.Black, fontSize = 30.sp))
+
+        }
+    }
+}
+
+
+@Composable
+fun RateCard(
+    painter: Painter,
+    contentDescription: String?,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 8.dp),
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp)
+
+        ) {
+            Image(
+                painter = painter,
+                modifier = Modifier.fillMaxWidth(),
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
